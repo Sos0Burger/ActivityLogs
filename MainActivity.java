3e0d14ad0663b3609fe304db8c478a96ea651e85
SosoBurger <stulnikovan1337@gmail.com>
2022-03-07 08:29:31 +0300

    ыыы

--- MainActivity.java
+++ MainActivity.java
@@ -13,8 +13,7 @@ import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
     private Integer counter = 0;
-    int scaling = 1;
-    int limit = 10;
+    private Integer limit = 10;
     public static final String TAG = "StartActivity";
 
     @Override
@@ -27,53 +26,65 @@ public class MainActivity extends AppCompatActivity {
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState != null &&
-                savedInstanceState.containsKey("count")) {
-            counter = savedInstanceState.getInt("count");
+                savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
         }
+        resetUI();
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onRestoreInstanceState");
     }
     public void onRestart(){
         super.onRestart();
         Log.d(TAG, "onRestart");
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
     }
     public void onStart(){
         super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onStart");
         resetUI();
     }
     public void onResume(){
         super.onResume();
         Log.d(TAG, "onResume");
+        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
     }
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("count", counter);
+        outState.putInt("counter", counter);
         Log.d(TAG, "onSaveInstanceState");
+        Toast.makeText(this, "onSaveInstance()", Toast.LENGTH_LONG).show();
     }
     public void onPause(){
         super.onPause();
         Log.d(TAG, "onPause");
+        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
     }
     public void onStop(){
         super.onStop();
         Log.d(TAG, "onStop");
+        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
     }
     public void onDestroy(){
         super.onDestroy();
         Log.d(TAG, "Destroye");
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
     }
     private void resetUI() {
         ((TextView) findViewById(R.id.textView)).setText(counter.toString());
         Log.d(TAG, "resetUI");
+        Toast.makeText(this, "ResetUI()", Toast.LENGTH_LONG).show();
     }
+
+
     public void onButtonAdd(View view){
-        counter = counter + 1*scaling;
+        counter++;
         TextView counterView = (TextView) findViewById(R.id.textView);
         TextView FigureOval = (TextView) findViewById(R.id.textView2);
         if (counter >= limit){
             ViewGroup.LayoutParams ovalparams = counterView.getLayoutParams();
-            ovalparams.width = (FigureOval.getWidth() + 25);
-            ovalparams.height = (FigureOval.getHeight() + 25);
+            ovalparams.width = (FigureOval.getWidth() + 50);
+            ovalparams.height = (FigureOval.getHeight() + 50);
             FigureOval.setLayoutParams(ovalparams);
             limit *= 10;
         }
