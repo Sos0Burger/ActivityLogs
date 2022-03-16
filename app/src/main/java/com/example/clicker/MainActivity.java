d5db539580d052da089e2746f75dacefa2c207f9
SosoBurger <stulnikovan1337@gmail.com>
2022-03-16 18:59:01 +0300

    All project

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -80,14 +80,6 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonAdd(View view){
         counter++;
         TextView counterView = (TextView) findViewById(R.id.textView);
-        TextView FigureOval = (TextView) findViewById(R.id.textView2);
-        if (counter >= limit){
-            ViewGroup.LayoutParams ovalparams = counterView.getLayoutParams();
-            ovalparams.width = (FigureOval.getWidth() + 50);
-            ovalparams.height = (FigureOval.getHeight() + 50);
-            FigureOval.setLayoutParams(ovalparams);
-            limit *= 10;
-        }
         counterView.setText(Integer.toString(counter));
     }
 
